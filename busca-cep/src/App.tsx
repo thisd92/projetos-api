e20a351cc696cba1e0bc2df8b41dda7d0839527c
thisd92 <thiiagodutra@gmail.com>
2022-07-25 17:24:44 -0300

    logo dev-th

--- busca-cep/src/App.tsx
+++ busca-cep/src/App.tsx
@@ -1,12 +1,16 @@
 import Search from './components/Search';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   
   return (
-    <main>
+    <>
+      <Header />
+      <main>
       <Search />
-    </main>
+      </main>
+    </>
   );
 };
 
---
+++ busca-cep/src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return(
+        <div className="container">
+            <img src="../assets/imgs/LogoDevTH.png" alt="" />
+        </div>
+    )
+}
+
+export default Header;
